a2c21e56ae508704775ac3744c3b55fb4c7a2aab
Palazzini, Francesco <palazzini.francesco@gmail.com>
2018-09-02 15:50:00 +0800

    ref: add enum errors

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,62 @@ use std::process;
 use std::env;
 use std::net::{IpAddr, ToSocketAddrs, SocketAddr, TcpStream};
 use std::time::Duration;
-use std::net::Ipv4Addr;
+use std::fmt;
+use std::error::Error;
 
 const MIN_PORT_NUMBER : u16 = 0;
 const MAX_PORT_NUMBER : u16 = 65535;
 const TIMEOUT_SEC : u64 = 1;
 const TIMEOUT_MS : u32 = 800;
 
+#[derive(Debug)]
 enum ResolveHostResult {
-    Success,
     Error,
     GetIpError
 }
 
-enum ResolvePortResult {
-    Success,
-    Error
+impl fmt::Display for ResolveHostResult {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            ResolveHostResult::Error => f.write_str("Could not resolve hostname."),
+            ResolveHostResult::GetIpError => f.write_str("Could not get ip."),
+        }
+    }
 }
+
+impl Error for ResolveHostResult {
+    fn description(&self) -> &str {
+        match *self {
+            ResolveHostResult::Error => "Could not resolve hostname.",
+            ResolveHostResult::GetIpError => "Could not get ip.",
+        }
+    }
+}
+
+#[derive(Debug)]
+enum ResolvePortResult {
+    Error,
+    OutOfRange
+}
+
+impl fmt::Display for ResolvePortResult {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            ResolvePortResult::Error => f.write_str("Invalid port."),
+            ResolvePortResult::OutOfRange => f.write_str("Port out of range."),
+        }
+    }
+}
+
+impl Error for ResolvePortResult {
+    fn description(&self) -> &str {
+        match *self {
+            ResolvePortResult::Error => "Invalid port.",
+            ResolvePortResult::OutOfRange => "Port out of range.",
+        }
+    }
+}
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -31,29 +70,25 @@ fn main() {
     }
 
     if args.len() > 2 {
-        match resolve_port(&args[2], &mut scan_port) {
-            ResolvePortResult::Success => {},
-            ResolvePortResult::Error => {
-                println!("Invalid port.");
-                process::exit(1);
-            }
-        }
+
+        scan_port = match resolve_port(&args[2]) {
+                Ok(port) => port,
+                Err(err) => {
+                    println!("Error: {}", err);
+                    process::exit(1);
+                }
+        };
     }
 
     let mut open_tcp_ports : Vec<u16> = Vec::with_capacity(65535);
-    let mut ip_addr : IpAddr = IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0));
-
-    match resolve_host(host, &mut ip_addr) {
-        ResolveHostResult::Success => {},
-        ResolveHostResult::Error => {
-            println!("Could not resolve hostname.");
-            process::exit(1);
-        },
-        ResolveHostResult::GetIpError => {
-            println!("Could not get ip.");
+    let ip_addr = match resolve_host(host) {
+        Ok(ip) => ip,
+        Err(err) => {
+            println!("Error: {}", err);
             process::exit(1);
         }
-    }
+    };
+
 
     let timeout = Duration::new(TIMEOUT_SEC, TIMEOUT_MS);
 
@@ -69,38 +104,33 @@ fn main() {
     println!("open tcp ports: {:#?}", open_tcp_ports);
 }
 
-fn resolve_host(host: &str, ip_addr: &mut IpAddr) -> ResolveHostResult {
+fn resolve_host(host: &str) -> Result<IpAddr, ResolveHostResult> {
     let mut resolved_ips = match (host, 0).to_socket_addrs() {
         Ok(ips) => ips,
-        Err(_) => return ResolveHostResult::Error
+        Err(_) => return Err(ResolveHostResult::Error)
     };
 
-    *ip_addr = match resolved_ips.next() {
-        Some(socket_addr) => socket_addr.ip(),
-        None => return ResolveHostResult::GetIpError
-    };
-
-    ResolveHostResult::Success
+    match resolved_ips.next() {
+        Some(socket_addr) => Ok(socket_addr.ip()),
+        None => Err(ResolveHostResult::GetIpError)
+    }
 }
 
-fn resolve_port(port_str: &str, scan_port: &mut u16) -> ResolvePortResult {
-    *scan_port = match port_str.parse::<u16>() {
-        Ok(port) => {
-            port
-        },
-        Err(_) => return ResolvePortResult::Error
+fn resolve_port(port_str: &str) -> Result<u16, ResolvePortResult> {
+    match port_str.parse::<u16>() {
+        Ok(port) => { 
+            if port < MIN_PORT_NUMBER || port > MAX_PORT_NUMBER {
+                return Err(ResolvePortResult::OutOfRange)
+            }
+            return Ok(port)
+        }
+        Err(_) => return Err(ResolvePortResult::Error)
     };
-
-    if *scan_port < MIN_PORT_NUMBER || *scan_port > MAX_PORT_NUMBER {
-        return ResolvePortResult::Error
-    }
-
-    ResolvePortResult::Success
 }
 
 fn do_scan(open_tcp_ports: &mut Vec<u16>, timeout: Duration, ip_addr: IpAddr, port: u16){
     let socket = SocketAddr::new(ip_addr, port);
     if let Ok(_) = TcpStream::connect_timeout(&socket, timeout) {
         open_tcp_ports.push(socket.port())
-    } 
+    }
 }
